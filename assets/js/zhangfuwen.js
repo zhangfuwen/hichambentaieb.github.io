3d8f009ae3ba5f808b7e6a08090b6c33e95830a2
Dean <Dean.Sinaean@gmail.com>
2022-05-08 21:31:16 +0800

    update

---
+++ assets/js/zhangfuwen.js
@@ -0,0 +1,56 @@
+
+function DoModal(but, contentNode) {
+    but.click(function(){
+        $(".Modal").html(contextNode);
+        $(".Modal").fadeToggle();
+    });
+
+}
+
+$(function () {
+    $("pre").each(function () {
+        var x = $(this);
+        let h = $(this).css("height");
+        if (h != "100px") {
+
+            x.css("max-height", 100);
+            x.css("height", 100);
+
+            let butExpand = $("<button>click to expand</button>").appendTo($(this).parent());
+            let butCollapse = $("<button>click to collapse</button>").prependTo(x.parent());
+            butExpand.show();
+            butCollapse.hide();
+
+            butExpand.click(function () {
+                x.css("max-height", "none");
+                x.css("height", "auto");
+                console.log("height", x.css("height"));
+                console.log("max", x.css("max-height"));
+                butExpand.hide();
+                butCollapse.show();
+            });
+
+            butCollapse.click(function () {
+                x.css("max-height", "100px");
+                x.css("height", "100px");
+                console.log("height", x.css("height"));
+                console.log("max", x.css("max-height"));
+                butCollapse.hide();
+                butExpand.show();
+            });
+        }
+    });
+
+    $(".Modal").hide();
+    $(".Close").click(function(){
+      $(".Modal").fadeOut();
+    });
+
+    $("pre").each(function() {
+        var x = $(this);
+        var code = x.child("code");
+        let butModel = $("<button> Maximize</button>").appendTo(x.parent());
+        DoModal(butModel, code.text());
+    });
+
+});
